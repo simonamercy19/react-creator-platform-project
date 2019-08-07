22db5c783f64b242ea5e2e45d51d7e2ebd8b8a33
Simona <simona.diniz@bidchat.com>
2019-08-07 16:38:12 +0530

    Updates

---
+++ precache-manifest.a73c3b69c13fd281c2cffbf530c67270.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8511d860782dbb8058e421c7d804e871",
+    "url": "/react-creator-platform-project/index.html"
+  },
+  {
+    "revision": "4790014c2ec895f2bacc",
+    "url": "/react-creator-platform-project/static/css/main.dd0d9a4d.chunk.css"
+  },
+  {
+    "revision": "276638664ece2d258665",
+    "url": "/react-creator-platform-project/static/js/2.cdaf74fd.chunk.js"
+  },
+  {
+    "revision": "4790014c2ec895f2bacc",
+    "url": "/react-creator-platform-project/static/js/main.c18c9517.chunk.js"
+  },
+  {
+    "revision": "680fa4b2732cc53d5ed3",
+    "url": "/react-creator-platform-project/static/js/runtime~main.96d5f78f.js"
+  },
+  {
+    "revision": "2d15decc87393ec398f00dc3d7a2163d",
+    "url": "/react-creator-platform-project/static/media/Lato-Bold.2d15decc.ttf"
+  },
+  {
+    "revision": "c5eb54404c9dbda925f7142d6343c913",
+    "url": "/react-creator-platform-project/static/media/Lato-Regular.c5eb5440.ttf"
+  },
+  {
+    "revision": "8a6b453009980d3779de852ea384c18c",
+    "url": "/react-creator-platform-project/static/media/Lato-Thin.8a6b4530.ttf"
+  },
+  {
+    "revision": "7c18188784f21915f42a5b3bc9d91e20",
+    "url": "/react-creator-platform-project/static/media/Roboto-Bold.7c181887.ttf"
+  },
+  {
+    "revision": "d52f011be65b281ba8ca1c3f689cf133",
+    "url": "/react-creator-platform-project/static/media/Roboto-Medium.d52f011b.ttf"
+  },
+  {
+    "revision": "18d44f79b3979ec168862093208c6d7d",
+    "url": "/react-creator-platform-project/static/media/Roboto-Regular.18d44f79.ttf"
+  },
+  {
+    "revision": "2f0faf219d4b7a06bca4ae9d5cb21fc7",
+    "url": "/react-creator-platform-project/static/media/icomoon.2f0faf21.svg"
+  },
+  {
+    "revision": "3c8e45ddb759e54d6f8396630a02bd29",
+    "url": "/react-creator-platform-project/static/media/icomoon.3c8e45dd.woff"
+  },
+  {
+    "revision": "8ed6467fe9c26437da878a50be379687",
+    "url": "/react-creator-platform-project/static/media/icomoon.8ed6467f.ttf"
+  },
+  {
+    "revision": "a9756eaa8773b76b0bca4b3ce006f6be",
+    "url": "/react-creator-platform-project/static/media/icomoon.a9756eaa.eot"
+  },
+  {
+    "revision": "31d9d1285aad515ba55a751e9dc0833b",
+    "url": "/react-creator-platform-project/static/media/merch-mock.31d9d128.png"
+  },
+  {
+    "revision": "0e841f42253e3a0cde4b4a6b6c4ef80d",
+    "url": "/react-creator-platform-project/static/media/profile_pic.0e841f42.jpeg"
+  },
+  {
+    "revision": "1e7735c73d52ad446d2424a9b7819179",
+    "url": "/react-creator-platform-project/static/media/request-img.1e7735c7.svg"
+  },
+  {
+    "revision": "9a5a459f50f251735fe2bac1e70f11a3",
+    "url": "/react-creator-platform-project/static/media/shop-item1.9a5a459f.jpg"
+  },
+  {
+    "revision": "8d6924322af9e38981c711c891b0cd5d",
+    "url": "/react-creator-platform-project/static/media/ticket-new.8d692432.jpg"
+  },
+  {
+    "revision": "8e5ac0eaca35d68cea2ec70b6ac74d35",
+    "url": "/react-creator-platform-project/static/media/user_profile.8e5ac0ea.jpg"
+  }
+]);
